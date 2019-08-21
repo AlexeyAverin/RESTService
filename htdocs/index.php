16ba77f76545b7ca7caf7d9f6eb4c9121d5ece78
Alexey <bercana@mail.ru>
2019-08-21 09:11:42 +0300

    Доброе утроgit add htdocs/! Начинаемgit add htdocs/!

---
+++ htdocs/index.php
@@ -0,0 +1,51 @@
+<?php
+
+
+
+ini_set('error_reporting', E_ALL);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+
+
+
+
+
+
+
+
+
+echo "<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset='utf-8'>
+        <title>RESTService</title>
+        <link rel='stylesheet' type='text/css' href='/css/styles.css'>
+    </head>
+    <body>
+
+        <header>
+            <div class='logotip'>RST</div>
+            <div class='company'>Добрый день!!!</div>
+            <div class='menu'>";
+
+
+echo "
+            </div>
+        </header>
+
+        <div class='contents'>
+            <div> </div>
+        ";
+
+
+echo "
+        </div>
+        <div class='contacts'>
+            <div class='contactsItems'>Российская Федерация</div>
+            <div class='contactsItems'>+7 (123) 123-12-12</div>
+            <div class='contactsItems'>Все права защищены</div>
+        </div>
+    </body>
+</html>";
+
+?>
